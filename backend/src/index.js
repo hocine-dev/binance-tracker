b3d0136090839268793e45b346dfa249985cf0e6
hocinedev <hocinedev4@gmail.com>
2024-06-29 14:14:07 +0100

    ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
