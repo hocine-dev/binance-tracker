35ab07b818990bd0911290befc5ab50e214156dd
hocinedev <hocinedev4@gmail.com>
2024-06-29 11:39:53 +0100

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var checkbox = document.getElementById("emailSent");
 // Function to fetch ratio data
 async function fetchRatio() {
   try {
-    const response = await fetch("https://tizi-industrie.com/api/ratio");
+    const response = await fetch("https://tizi-industrie.com/backend/src/routes/api.js");
     const data = await response.json();
     return data.ratio;
   } catch (error) {
